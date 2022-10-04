75da8436fd0e3106f4d69c50ca878a3e615e6cdb
bakelit69 <113536297+bakelit69@users.noreply.github.com>
2022-10-04 08:50:43 +0200

    Update Comparator.cs

--- Open-Lab-03.05/Comparator.cs
+++ Open-Lab-03.05/Comparator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._05
 {
@@ -6,7 +6,9 @@ namespace Open_Lab_03._05
     {
         public bool MatchCaseInsensitive(string str1, string str2)
         {
-            throw new NotImplementedException();
+            
+            return str1.Equals(str2, StringComparison.CurrentCultureIgnoreCase);
+            
         }
     }
 }
